7f55000e8b1dc3d4df982a2c50c25da913fb4458
BeqaBND <b.baindurashvili77@gmail.com>
2023-08-28 16:55:22 +0400

    updated style.css added icons and updated javascypt

---
+++ javascrypt/scrypt.js
@@ -0,0 +1,60 @@
+function initSlider() {
+  const slides = document.querySelectorAll(".slide");
+  const sliderWrapper = document.querySelector(".slider-wrapper");
+
+  let activeIndex = 0;
+
+  function renderSlides() {
+    slides.forEach((slide, index) => {
+      if (activeIndex === index) {
+        slide.classList.add("active");
+      } else {
+        slide.classList.remove("active");
+      }
+    });
+  }
+  renderSlides();
+  function showNext() {
+    if (activeIndex === slides.length - 1) {
+      activeIndex = 0;
+    } else {
+      activeIndex++;
+    }
+    renderSlides();
+  }
+
+  let sliderIntervalId = null;
+
+  function startAutoSliderFn() {
+    sliderIntervalId = setInterval(showNext, 5000);
+  }
+
+  startAutoSliderFn();
+}
+
+initSlider();
+
+let commentsSlides = document.querySelectorAll(".comments-slide");
+let commentsButton1 = document.getElementById("comments-button-1");
+let commentsButton2 = document.getElementById("comments-button-2");
+let commentsButton3 = document.getElementById("comments-button-3");
+
+commentsSlides[0].classList.add("active");
+
+commentsButton1.addEventListener("click", function () {
+  commentsSlides[0].classList.add("active");
+  commentsSlides[1].classList.remove("active");
+  commentsSlides[2].classList.remove("active");
+});
+
+commentsButton2.addEventListener("click", function () {
+  commentsSlides[0].classList.remove("active");
+  commentsSlides[1].classList.add("active");
+  commentsSlides[2].classList.remove("active");
+});
+
+commentsButton3.addEventListener("click", function () {
+  commentsSlides[0].classList.remove("active");
+  commentsSlides[1].classList.remove("active");
+  commentsSlides[2].classList.add("active");
+});
